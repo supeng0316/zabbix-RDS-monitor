c4bfab24df37d8f1136a8f8165048d39841e1701
supeng0316 <iisupeng@163.com>
2018-08-29 13:48:52 +0800

    Update discovery_rds.py
    当实例数量大于一页时，该脚本只能查询出第一页RDS实例。
    
    故做出如上修改。

--- discovery_rds.py
+++ discovery_rds.py
@@ -10,24 +10,36 @@ RegionId = 'cn-shenzhen'
 
 clt = client.AcsClient(ID,Secret,RegionId)
 
+
 DBInstanceIdList = []
 DBInstanceIdDict = {}
 ZabbixDataDict = {}
 def GetRdsList():
-    RdsRequest = DescribeDBInstancesRequest.DescribeDBInstancesRequest()
-    RdsRequest.set_accept_format('json')
-    #RdsInfo = clt.do_action(RdsRequest)
-    RdsInfo = clt.do_action_with_exception(RdsRequest)
-    for RdsInfoJson in (json.loads(RdsInfo))['Items']['DBInstance']:
-        DBInstanceIdDict = {}
-        try:
-            DBInstanceIdDict["{#DBINSTANCEID}"] = RdsInfoJson['DBInstanceId']
-            DBInstanceIdDict["{#DBINSTANCEDESCRIPTION}"] = RdsInfoJson['DBInstanceDescription']
-            DBInstanceIdList.append(DBInstanceIdDict)
-        except Exception, e:
-            print Exception, ":", e
-            print "Please check the RDS alias !Alias must not be the same as DBInstanceId！！！"
-            
+    PageNumberStep = 1
+    while 1:  //添加循环
+        RdsRequest = DescribeDBInstancesRequest.DescribeDBInstancesRequest()
+        RdsRequest.set_PageSize(100)    //设置当前page页显示实例数量为100
+        RdsRequest.set_accept_format('json')
+        RdsRequest.set_PageNumber(PageNumberStep)     //查询指定page的数据库实例
+        RdsInfo = clt.do_action_with_exception(RdsRequest)
+        RdsInfoList = (json.loads(RdsInfo))['Items']['DBInstance']
+        PageNumberStep += 1
+
+        # print len(RdsInfoList)
+        if len(RdsInfoList) == 0:
+            break
+
+        for RdsInfoJson in RdsInfoList:
+            DBInstanceIdDict = {}
+            try:
+                DBInstanceIdDict["{#DBINSTANCEID}"] = RdsInfoJson['DBInstanceId']
+                DBInstanceIdDict["{#DBINSTANCEDESCRIPTION}"] = RdsInfoJson['DBInstanceDescription']
+                DBInstanceIdList.append(DBInstanceIdDict)
+                # print DBInstanceIdDict
+            except Exception, e:
+                print Exception, ":", e
+                print "Please check the RDS alias !Alias must not be the same as DBInstanceId！！！"
+
 
 
 GetRdsList()
